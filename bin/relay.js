4ba3ca79e6c6b012d2585e1426de560b140cc779
Aakil Fernandes <aakilfernandes@gmail.com>
2017-10-27 18:09:16 -0400

    drop bin/keypair and use privateKeyJson

--- bin/relay.js
+++ bin/relay.js
@@ -3,12 +3,8 @@ const Keypair = require('../lib/classes/Keypair')
 const commander = require('commander')
 const _ = require('lodash')
 
-commander.version('0.0.0').arguments('<privateKey> [port] [address]').action((privateKeyHex, port = 5000, address = '0.0.0.0') => {
-  const privateKey = new Uint8Array(_.range(32).map((index) => {
-    const hexIndex = index * 2
-    const byteHex = privateKeyHex.slice(hexIndex, hexIndex + 2)
-    return parseInt(byteHex, 16)
-  }))
+commander.version('0.0.0').arguments('<privateKeyJson> [port] [address]').action((privateKeyJson, port = 5000, address = '0.0.0.0') => {
+  const privateKey = new Uint8Array(JSON.parse(privateKeyJson))
 
   const keypair = new Keypair(privateKey)
   const relay = new Relay(keypair)
